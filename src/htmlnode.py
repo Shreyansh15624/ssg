053eede8978523b277f9b1192fb7aab0d1227884
Shreyansh15624 <shreyanshvajroju@gmail.com>
2025-12-16 14:38:42 +0000

    Created & tested child class ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,6 +21,23 @@ class HTMLNode():
             return (self.tag == other.tag) and (self.value == other.value) and (self.children == other.children) and (self.props == other.props)
         return False
     
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children=children, props=props)
+    
+    def __eq__(self, other):
+        if isinstance(other, ParentNode):
+            return (self.tag == other.tag) and (self.children == other.children) and (self.props == other.props)
+        return False
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("tag is missing")
+        if self.children is None:
+            raise ValueError("children is missing")
+        children_html = "".join(map(lambda x: x.to_html(), self.children))
+        return f"<{self.tag}>{children_html}</{self.tag}>"
+
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, children=None, props=props)
