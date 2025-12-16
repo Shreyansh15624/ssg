c7a802300195ec6fc2b45d471f95c22bbe713803
Shreyansh15624 <shreyanshvajroju@gmail.com>
2025-12-16 13:53:05 +0000

    Created & tested child class LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,9 +9,9 @@ class HTMLNode():
         raise NotImplementedError()
     
     def props_to_html(self):
-        if self.props in (None, ""):
+        if self.props is None:
             return ""
-        return f"href:{self.props["href"]} target={self.props["target"]}"
+        return f' href="{self.props["href"]}" target="{self.props["target"]}"'
         
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
@@ -20,3 +20,20 @@ class HTMLNode():
         if isinstance(other, HTMLNode):
             return (self.tag == other.tag) and (self.value == other.value) and (self.children == other.children) and (self.props == other.props)
         return False
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, children=None, props=props)
+        
+    def __eq__(self, other):
+        if isinstance(other, LeafNode):
+            return (self.tag == other.tag) and (self.value == other.value) and (self.props == other.props)
+        return False
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("value is missing")
+        elif not self.tag:
+            return self.value
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -19,7 +19,7 @@ class TestHTMLNode(unittest.TestCase):
         # Your code formats it as: "href:url target=target"
         self.assertEqual(
             node.props_to_html(),
-            "href:https://www.google.com target=_blank",
+            ' href="https://www.google.com" target="_blank"',
         )
 
     def test_values(self):
@@ -74,6 +74,32 @@ class TestHTMLNode(unittest.TestCase):
         # Test comparing with a different type entirely
         node = HTMLNode("p", "text")
         self.assertNotEqual(node, "some string")
+        
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_to_html_no_tag(self):
+        # A node without a tag should just return the raw text value
+        node = LeafNode(None, "Just raw text.")
+        self.assertEqual(node.to_html(), "Just raw text.")
+
+    def test_to_html_with_props(self):
+        # Test rendering with HTML attributes (props)
+        props = {"href": "https://www.google.com", "target": "_blank"}
+        node = LeafNode("a", "Click me!", props)
+        self.assertEqual(node.to_html(),'<a href="https://www.google.com" target="_blank">Click me!</a>')
+
+    # This test is crucial! 
+    # Logic usually dictates that a LeafNode MUST have a value to be valid.
+    def test_to_html_no_value(self):
+        node = LeafNode("p", None)
+        # We verify that calling to_html() raises a specific error
+        # if the value is missing.
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 
 if __name__ == "__main__":
