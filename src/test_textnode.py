784146a1f3cc46955b4f8d85446ab706080f0780
Shreyansh15624 <shreyanshvajroju@gmail.com>
2025-12-17 06:54:55 +0000

    Split Delimiter & Links Extractor have been added

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
 class TestTextNode(unittest.TestCase):
@@ -21,10 +21,10 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.value, "This is a text node")
 
     def test_bold(self):
-            node = TextNode("This is bold", TextType.BOLD)
-            html_node = text_node_to_html_node(node)
-            self.assertEqual(html_node.tag, "b")
-            self.assertEqual(html_node.value, "This is bold")
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
 
     def test_italic(self):
         node = TextNode("This is italic", TextType.ITALIC)
@@ -54,6 +54,57 @@ class TestTextNode(unittest.TestCase):
             html_node.props,
             {"src": "https://www.boot.dev/image.png", "alt": "This is an image"},
         )
+    
+    def test_delim_bold(self):
+        node = TextNode("This is text with a **bolded** word", TextType.PLAIN)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+            [
+                TextNode("This is text with a ", TextType.PLAIN),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word", TextType.PLAIN),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_double(self):
+        node = TextNode(
+            "This is text with a **bolded** word and **another**", TextType.PLAIN
+        )
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+            [
+                TextNode("This is text with a ", TextType.PLAIN),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word and ", TextType.PLAIN),
+                TextNode("another", TextType.BOLD),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.PLAIN)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            [
+                TextNode("This is text with a ", TextType.PLAIN),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.PLAIN),
+            ],
+            new_nodes,
+        )
+        
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+        
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://www.example.com)"
+        )
+        self.assertListEqual([("link", "https://www.example.com")], matches)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from htmlnode import LeafNode
+import re
+
 class TextType(Enum):
     PLAIN = "text"
     BOLD = "bold"
@@ -22,6 +24,8 @@ class TextNode():
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
+
+    
 def text_node_to_html_node(text_node):
     match text_node.text_type:
         case TextType.PLAIN:
@@ -38,3 +42,31 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("text_node error")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    final_list = []
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN:
+            final_list.append(node)
+            continue
+        split_values = node.text.split(delimiter)
+        if len(split_values) % 2 == 0:
+            raise Exception("unmatched delimiters")
+        for i in range(len(split_values)):
+            if split_values[i] == "":
+                continue
+            elif i % 2 != 0:
+                res1 = TextNode(split_values[i], text_type)
+                final_list.append(res1)
+            else:
+                res2 = TextNode(split_values[i], TextType.PLAIN)
+                final_list.append(res2)
+    return final_list
+
+def extract_markdown_images(text):
+    res = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return res
+
+def extract_markdown_links(text):
+    res = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return res
